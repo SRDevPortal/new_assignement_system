80527b206e1013e602b9021f2d2c8d8c7f365013
SRDevPortal <webdevelopersriaas@gmail.com>
2026-07-02 13:39:42 +0530

    Update assignment system install settings

--- new_assignement_system/install.py
+++ new_assignement_system/install.py
@@ -16,4 +16,4 @@ def after_migrate() -> None:
 		settings = frappe.get_single("New Assignement System Settings")
 		if not settings.enabled:
 			settings.enabled = 1
-		settings.save(ignore_permissions=True)
+		settings.save(ignore_permissions=True, ignore_links=True)
